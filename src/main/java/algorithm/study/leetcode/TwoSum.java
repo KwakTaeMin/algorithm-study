a5c710794f0daddaf6500250a702066087abdc51
taemin <taminging@kakao.com>
2021-08-20 23:03:45 +0900

    twosum end

--- src/main/java/algorithm/study/leetcode/TwoSum.java
+++ src/main/java/algorithm/study/leetcode/TwoSum.java
@@ -1,4 +1,18 @@
 package algorithm.study.leetcode;
 
 public class TwoSum {
+
+	public int[] twoSum(int[] input, int target) {
+
+		for (int index = 0; index < input.length; index++) {
+			int minusTargetValue = target - input[index];
+			for (int search = index; search < input.length; search++) {
+				if (minusTargetValue == input[search] && index != search) {
+					int[] result = {index, search};
+					return result;
+				}
+			}
+		}
+		return null;
+	}
 }
--- src/test/java/algorithm/study/leetcode/TwoSumTest.java
+++ src/test/java/algorithm/study/leetcode/TwoSumTest.java
@@ -1,4 +1,32 @@
 package algorithm.study.leetcode;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class TwoSumTest {
+
+	@Test
+	void twoSumTest1() {
+		TwoSum algorithm = new TwoSum();
+		int[] input = {2, 7, 11, 15};
+		int[] expect = {0, 1};
+		assertThat(algorithm.twoSum(input, 9)).isEqualTo(expect);
+	}
+
+	@Test
+	void twoSumTest2() {
+		TwoSum algorithm = new TwoSum();
+		int[] input = {3, 2, 4};
+		int[] expect = {1, 2};
+		assertThat(algorithm.twoSum(input, 6)).isEqualTo(expect);
+	}
+
+	@Test
+	void twoSumTest3() {
+		TwoSum algorithm = new TwoSum();
+		int[] input = {3, 3};
+		int[] expect = {0, 1};
+		assertThat(algorithm.twoSum(input, 6)).isEqualTo(expect);
+	}
 }
